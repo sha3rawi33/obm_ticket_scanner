0e6ea73d8905ebb2b35ac36f524b4ef225e956e5
Ahmed Saad Sharawi <saad1262002@gmail.com>
2021-07-19 13:48:29 +0200

    UI customization + vibration on end

--- lib/main.dart
+++ lib/main.dart
@@ -6,26 +6,124 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:qr_code_scanner/qr_code_scanner.dart';
 import 'package:http/http.dart' as http;
+import 'package:vibration/vibration.dart';
 
-void main() => runApp(MaterialApp(home: MyHome()));
+var apiURL = "http://gradesaver.xyz/api/users/event";
+ThemeData dark = ThemeData(
+    // colorScheme: ColorScheme.dark(),
+    scaffoldBackgroundColor: Color(0xFF102334),
+    primaryColor: Color(0xFF1d91f4),
+    accentColor: Colors.white);
+
+void main() => runApp(MaterialApp(
+      home: MyHome(),
+      themeMode: ThemeMode.dark,
+      darkTheme: dark,
+      debugShowCheckedModeBanner: false,
+    ));
 
 class MyHome extends StatelessWidget {
   const MyHome({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Flutter Demo Home Page')),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.of(context).push(MaterialPageRoute(
-              builder: (context) => QRViewExample(),
-            ));
-          },
-          child: Text('qrView'),
+    return Stack(
+      children: [
+        Image.asset("assets/back.jpg", height: MediaQuery.of(context).size.height, width: MediaQuery.of(context).size.width, fit: BoxFit.cover),
+        Scaffold(
+          backgroundColor: Colors.transparent,
+          appBar: AppBar(
+            toolbarHeight: 70,
+            title: Text(
+              'BYF ticket checker',
+              style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+            ),
+            centerTitle: true,
+            backgroundColor: Colors.transparent,
+          ),
+          body: Center(
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                      height: 200,
+                      child: Image.asset(
+                        'assets/obm_logo.png',
+                      )),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(40, 30, 40, 10),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Align(
+                          alignment: Alignment.bottomLeft,
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(
+                              "Enter API URL",
+                              style: TextStyle(color: Colors.white, fontSize: 12),
+                            ),
+                          ),
+                        ),
+                        TextFormField(
+                          style: TextStyle(
+                              color: Color(
+                                0xFFFFFFFF,
+                              ),
+                              fontSize: 15),
+                          autofillHints: [AutofillHints.url],
+                          decoration: InputDecoration(
+                            enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                              borderSide: BorderSide(color: Color(0xFF5F85DB)),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                              borderSide: BorderSide(color: Color(0xFF14faa2)),
+                            ),
+                            filled: true,
+                            hintStyle: TextStyle(color: Color(0xFF509b8f), fontSize: 10),
+                            hintText: "eg: https://plebits.com/API/v2/",
+                            fillColor: Color(0xFF1a2835),
+                            prefixIcon: Icon(
+                              Icons.link,
+                              color: Colors.white,
+                            ),
+                            contentPadding: EdgeInsets.symmetric(vertical: 5),
+                          ),
+                          keyboardType: TextInputType.emailAddress,
+                          onChanged: (value) {
+                            apiURL = value.trim();
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                  MaterialButton(
+                    color: Colors.blueAccent,
+                    highlightColor: Colors.red,
+                    enableFeedback: true,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    onPressed: () {
+                      Navigator.of(context).push(MaterialPageRoute(
+                        builder: (context) => QRViewExample(),
+                      ));
+                    },
+                    child: Text(
+                      'Start',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
         ),
-      ),
+      ],
     );
   }
 }
@@ -44,6 +142,9 @@ class _QRViewExampleState extends State<QRViewExample> {
     color: Colors.white,
   );
   Color buttonColor = Colors.blueAccent;
+  bool flashOn = false;
+  bool frontCam = false;
+  bool isScanning = true;
 
   // In order to get hot reload to work we need to pause the camera if the platform
   // is android, or resume the camera if the platform is iOS.
@@ -61,41 +162,51 @@ class _QRViewExampleState extends State<QRViewExample> {
     return Scaffold(
       floatingActionButton: FloatingActionButton(backgroundColor: buttonColor, onPressed: () {}, child: status),
       bottomNavigationBar: BottomAppBar(
+        color: Color(0xFF1de6af),
         shape: CircularNotchedRectangle(),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
             IconButton(
-              icon: Icon(Icons.flash_on),
-              color: Colors.black,
+              icon: Icon(flashOn ? Icons.flash_off : Icons.flash_on),
+              color: Color(0xFF102334),
               onPressed: () async {
                 await controller?.toggleFlash();
-                setState(() {});
+                setState(() {
+                  flashOn = !flashOn;
+                });
               },
             ),
             IconButton(
-              icon: Icon(Icons.camera_front),
-              color: Colors.black,
+              icon: Icon(frontCam ? Icons.camera_front : Icons.camera_alt),
+              color: Color(0xFF102334),
               onPressed: () async {
                 await controller?.flipCamera();
-                setState(() {});
+                setState(() {
+                  frontCam = !frontCam;
+                });
               },
             ),
             SizedBox(
               width: 40,
             ),
             IconButton(
-              icon: Icon(Icons.stop),
-              color: Colors.black,
+              icon: Icon(isScanning ? Icons.stop : Icons.play_arrow),
+              color: Color(0xFF102334),
               onPressed: () async {
-                await controller?.pauseCamera();
+                setState(() {
+                  isScanning = !isScanning;
+                });
+                isScanning ? await controller?.resumeCamera() : await controller?.pauseCamera();
               },
             ),
             IconButton(
-              icon: Icon(Icons.play_arrow),
-              color: Colors.black,
-              onPressed: () async {
-                await controller?.resumeCamera();
+              icon: Icon(Icons.settings),
+              color: Color(0xFF102334),
+              onPressed: () {
+                Vibration.cancel();
+                controller?.dispose();
+                Navigator.pop(context);
               },
             ),
           ],
@@ -107,17 +218,14 @@ class _QRViewExampleState extends State<QRViewExample> {
           Expanded(flex: 4, child: _buildQrView(context)),
           Expanded(
             flex: 1,
-            child: FittedBox(
-              fit: BoxFit.contain,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  SizedBox(
-                    height: 10,
-                  ),
-                  // status,
-                ],
-              ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                SizedBox(
+                  height: 10,
+                ),
+                // status,
+              ],
             ),
           )
         ],
@@ -144,6 +252,7 @@ class _QRViewExampleState extends State<QRViewExample> {
   void _onQRViewCreated(QRViewController controller) {
     setState(() {
       this.controller = controller;
+      isScanning = false;
     });
     controller.scannedDataStream.listen((scanData) async {
       print("SCANNED: ${scanData.code}");
@@ -165,11 +274,13 @@ class _QRViewExampleState extends State<QRViewExample> {
 
   @override
   void dispose() {
+    Vibration.cancel();
     controller?.dispose();
     super.dispose();
   }
 
   Future checkCode(String? code, String? url) async {
+    Vibration.cancel();
     url = "http://gradesaver.xyz/api/users/event";
     try {
       String body = code!.replaceAll("code", "ticket");
@@ -182,6 +293,7 @@ class _QRViewExampleState extends State<QRViewExample> {
       final result = responseData['works'];
       print(result);
       setState(() {
+        isScanning = false;
         status = Icon(
           result == "True"
               ? Icons.add
@@ -199,6 +311,15 @@ class _QRViewExampleState extends State<QRViewExample> {
                     ? Colors.yellowAccent
                     : Colors.red;
       });
+
+      result == "True"
+          ? Vibration.vibrate(pattern: [300, 100, 300, 100])
+          : result == "Entered"
+              ? Vibration.vibrate(duration: 3000)
+              : result == "Hold"
+                  ? Vibration.vibrate(duration: 300)
+                  : Vibration.vibrate(pattern: [300, 50, 300, 100, 300, 50, 300, 100, 300, 50, 300, 100]);
+
       return result;
     } catch (e) {
       print(e);
